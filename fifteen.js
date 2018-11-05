653901bbb7ca437b8f4a62d35b63f2c189fa069f
Rijkaard <harrison.rijkaard@gmail.com>
2018-11-04 22:26:41 -0600

    working js but no hover function, or additional features

--- fifteen.js
+++ fifteen.js
@@ -3,7 +3,8 @@
 
 
 /** FUNCTION SECTION**/
-function setPuzzlePieces(w) {
+
+function setPuzzlePieces(w) {  //ADDS THE PUZZLEPIECE CLASS TO THE PIECES
 
     for (i = 0; i < w.length; i++) {
         w[i].classList.add("puzzlepiece");
@@ -14,7 +15,7 @@ function setPuzzlePieces(w) {
     
 }
 
-function arrangeImage(p) {
+function arrangeImage(p) { //PROPERLY SETS THE BACKGROUND IMAGE ON THE PIECES
                                     // X   Y
     p[0].style.backgroundPosition = "0 0";
     p[1].style.backgroundPosition = "-100px 0";
@@ -40,8 +41,10 @@ function arrangeImage(p) {
     
 }
 
-function switchPieces(p1, p2) {
-    if (p2.outerHTML.includes('id="empty"')) {
+function switchPieces(p1, p2) { //created to switch pieces when clicked and is next to an empty space , also used in shufling the pieces
+
+    if (p2.outerHTML.includes('id="empty"')) { 
+
         var x = [p1.className, p1.style.backgroundPosition, p1.style.position, p1.style.float, p1.innerHTML];
 
         p1.className = p2.className;  //switch class
@@ -59,12 +62,274 @@ function switchPieces(p1, p2) {
         p2.style.float = x[3];
         p2.innerHTML = x[4];
         p2.removeAttribute("id"); //removing the 'empty' id  from div
-    } else {
-        console.log("invalid move");
+
+    } else if (p1.outerHTML.includes('id="empty"')) {
+        var x = [p2.className, p2.style.backgroundPosition, p2.style.position, p2.style.float, p2.innerHTML];
+
+        p2.className = p1.className;    
+        p2.style.backgroundPosition = "";
+        p2.style.background = "none";    
+        p2.style.position = p1.style.position; 
+        p2.style.float = p1.style.float; 
+        p2.setAttribute("id", "empty"); 
+        p2.innerHTML = p1.innerHTML;
+
+        p1.className = x[0]; 
+        p1.removeAttribute("style"); 
+        p1.style.backgroundPosition = x[1]; 
+        p1.style.position = x[2];
+        p1.style.float = x[3];
+        p1.innerHTML = x[4];
+        p1.removeAttribute("id"); 
+    } else {                                                                    //CASE WHERE TILES ARE SHUFFLED
+
+        var x = [p1.className, p1.style.backgroundPosition, p1.style.position, p1.style.float, p1.innerHTML];
+
+        p1.className = p2.className;
+        p1.style.backgroundPosition = p2.style.backgroundPosition;; 
+        p1.style.position = p2.style.position; 
+        p1.style.float = p2.style.float; 
+        p1.innerHTML = p2.innerHTML;
+
+        p2.className = x[0]; 
+        p2.style.backgroundPosition = x[1];
+        p2.style.position = x[2];
+        p2.style.float = x[3];
+        p2.innerHTML = x[4];
     }
 }
 
-function checkEmpty(p , n) { //check if an empty space is next to a tileif clicked
+function addListener(pieces2) {
+
+    pieces2[0].onclick = function () {
+        if (pieces2[1].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[0], pieces2[1]);
+        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[0], pieces2[4]);
+        }
+    };
+
+    pieces2[1].onclick = function () {
+        if (pieces2[2].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[1], pieces2[2]);
+        } else if (pieces2[0].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[1], pieces2[0]);
+        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[1], pieces2[5]);
+        }
+    };
+
+    pieces2[2].onclick = function () {
+
+        if (pieces2[3].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[2], pieces2[3]);
+        } else if (pieces2[1].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[2], pieces2[1]);
+        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[2], pieces2[6]);
+        }
+    };
+
+    pieces2[3].onclick = function () {
+
+        if (pieces2[2].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[3], pieces2[2]);
+        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[3], pieces2[7]);
+        }
+    };
+
+    pieces2[4].onclick = function () { //5
+
+        if (pieces2[0].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[4], pieces2[0]);
+        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[4], pieces2[5]);
+        } else if (pieces2[8].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[4], pieces2[8]);
+        }
+    };
+
+    pieces2[5].onclick = function () {  //6
+
+        if (pieces2[1].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[5], pieces2[1]);
+        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[5], pieces2[6]);
+        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[5], pieces2[9]);
+        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[5], pieces2[4]);
+        }
+    };
+
+    pieces2[6].onclick = function () {  //7
+
+        if (pieces2[2].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[6], pieces2[2]);
+        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[6], pieces2[5]);
+        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[6], pieces2[7]);
+        } else if (pieces2[10].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[6], pieces2[10]);
+        }
+    };
+
+    pieces2[7].onclick = function () {  //8
+
+        if (pieces2[3].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[7], pieces2[3]);
+        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[7], pieces2[6]);
+        } else if (pieces2[11].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[7], pieces2[11]);
+        }
+    };
+
+    pieces2[8].onclick = function () {  //9
+
+        if (pieces2[9].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[8], pieces2[9]);
+        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[8], pieces2[4]);
+        } else if (pieces2[12].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[8], pieces2[12]);
+        }
+    };
+
+    pieces2[9].onclick = function () {  //10
+
+        if (pieces2[10].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[9], pieces2[10]);
+        } else if (pieces2[8].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[9], pieces2[8]);
+        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[9], pieces2[13]);
+        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[9], pieces2[5]);
+        }
+    };
+
+    pieces2[10].onclick = function () {  //11
+
+        if (pieces2[11].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[10], pieces2[11]);
+        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[10], pieces2[6]);
+        } else if (pieces2[14].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[10], pieces2[14]);
+        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[10], pieces2[9]);
+        }
+    };
+
+    pieces2[11].onclick = function () {  //12
+
+        if (pieces2[10].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[11], pieces2[10]);
+        } else if (pieces2[15].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[11], pieces2[15]);
+        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[11], pieces2[7]);
+        }
+    };
+
+    pieces2[12].onclick = function () {  //13
+
+        if (pieces2[8].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[12], pieces2[8]);
+        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[12], pieces2[13]);
+        }
+    };
+
+    pieces2[13].onclick = function () { //14
+        if (pieces2[14].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[13], pieces2[14]);
+        } else if (pieces2[12].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[13], pieces2[12]);
+        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[13], pieces2[9]);
+        }
+    };
+
+    pieces2[14].onclick = function () { //15
+        if (pieces2[15].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[14], pieces2[15]);
+        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[14], pieces2[13]);
+        } else if (pieces2[10].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[14], pieces2[10]);
+        }
+    };
+
+    pieces2[15].onclick = function () {         //when a tile moves into the bottom right corner
+        if (pieces2[14].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[15], pieces2[14]);
+        } else if (pieces2[11].outerHTML.includes('id="empty"')) {
+            switchPieces(pieces2[15], pieces2[11]);
+        }
+    };
+
+}
+
+function emptyEvn(p) {                  //FUNCTION TO HELP DETERMINE IF THE SHUFFLED PUZZLE IS SOLVABLE by checking the row in which the empty piece is in
+    var t = p.length;
+    for (i = 0; i < t; i++) {
+        if (p[i].outerHTML.includes('id="empty"') && i < 4) {
+            return true;
+        } else if (pieces2[2].outerHTML.includes('id="empty"') && i > 7 && i < 12) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+function shuffle(p) {  //SHUFFLES THE PIECES AND DETERMINES IF THE SHUFFLED PIECES ARE SOLVABLE
+    var term = p.length;
+    var term2 = term -1;
+    var inversions = 0;
+    
+    for (i = 0; i < term2; i++) {
+        var x = Math.floor(Math.random() * term2);
+        console.log('x:'+x);
+
+        if (p[x].outerHTML.includes('id="empty"')) {
+            switchPieces(p[i], p[x]);
+        } else {
+            switchPieces(p[i], p[x]);
+        }
+        
+        for (j = 0; j < term2-1; j++) {
+            if (parseInt(p[x].innerHTML) > parseInt(p[j].innerHTML)) {
+                inversions = inversions + 1;
+                
+            }
+        }
+        console.log(inversions);
+    }
+    
+     if (inversions % 2 == 0 && !emptyEvn(p)) {
+         console.log("solvable");
+         console.log("check1: " + !emptyEvn(p));
+         console.log("smhh2: " + emptyEvn(p));
+         console.log("smhh3: " + inversions % 2);
+
+     } else if (inversions % 2 == 1 && emptyEvn(p)) {
+         console.log("solvable");
+         console.log("smhh2"+ emptyEvn(p));
+     } else {
+        switchPieces(p[term2 - 1], p[term2]);
+        inversions = inversions + 1;
+        console.log("new after switching 1: "+inversions);
+    }
+       
+}
+/*
+function checkEmpty(p , n) { //check if an empty space is next to a tile if MOUSEOVER
     // odd cases = 1, (2,3,) 4 | ,5,8 | ,9,12, | 13,(14,15)
     switch (parseInt(p.innerHTML)) {
         case 1:
@@ -172,6 +437,7 @@ function checkEmpty(p , n) { //check if an empty space is next to a tileif click
 
 
 } 
+*/
 
 /** END FUNCTION**/
 
@@ -190,10 +456,11 @@ window.onload = function () {
 
     /**END VARIABLES**/
 
+
    
     
     setPuzzlePieces(pieces);        //arrange pieces in order in puzzle area
-    $('#puzzlearea').append('<div class="puzzlepiece" style="position: relative; float: left;" id ="empty"></div>');    //adds empty square to puzzle
+    $('#puzzlearea').append('<div class="puzzlepiece" style="position: relative; float: left;" id ="empty"> </div>');    //adds empty square to puzzle
     pieces2 = puzzleArea.querySelectorAll("div");        //pieces including the blank space
     arrangeImage(pieces2);
 
@@ -201,182 +468,12 @@ window.onload = function () {
         
     console.log(pieces2);
 
-    var x = [pieces2[14].className, pieces2[14].style.backgroundPosition, pieces2[14].style.position, pieces2[14].style.float, pieces2[14].innerHTML, pieces2[14].style.backgroundImage];
-    console.log(x);
-    
-    
-    
-    pieces2[0].onclick = function () {
-        if (pieces2[1].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[0], pieces2[1]);
-        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[0], pieces2[4]);
-        }
+    var button = document.getElementById("shufflebutton");
+    button.onclick = function () {
+        shuffle(pieces2);
     };
-
-    pieces2[1].onclick = function () {
-        if (pieces2[2].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[1], pieces2[2]);
-        } else if (pieces2[0].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[1], pieces2[0]);
-        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[1], pieces2[5]);
-        }
-    };
-
-    pieces2[2].onclick = function () {
-
-        if (pieces2[3].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[2], pieces2[3]);
-        } else if (pieces2[1].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[2], pieces2[1]);
-        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[2], pieces2[6]);
-        }
-    };
-
-    pieces2[3].onclick = function () {
-
-        if (pieces2[2].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[3], pieces2[2]);
-        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[3], pieces2[7]);
-        }
-    };
-
-    pieces2[4].onclick = function () { //5
-
-        if (pieces2[0].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[4], pieces2[0]);
-        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[4], pieces2[5]);
-        } else if (pieces2[8].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[4], pieces2[8]);
-        }
-    };
-
-    pieces2[5].onclick = function () {  //6
-
-        if (pieces2[1].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[5], pieces2[1]);
-        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[5], pieces2[6]);
-        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[5], pieces2[9]);
-        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces[5], pieces2[4]);
-        }
-    };
-
-    pieces2[6].onclick = function () {  //7
-
-        if (pieces2[2].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[6], pieces2[2]);
-        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[6], pieces2[5]);
-        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[6], pieces2[7]);
-        } else if (pieces2[10].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces[6], pieces2[10]);
-        }
-    };
-    
-    pieces2[7].onclick = function () {  //8
-
-        if (pieces2[3].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[7], pieces2[3]);
-        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[7], pieces2[6]);
-        } else if (pieces2[11].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[7], pieces2[11]);
-        } 
-    };
-
-    pieces2[8].onclick = function () {  //9
-
-        if (pieces2[9].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[8], pieces2[9]);
-        } else if (pieces2[4].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[8], pieces2[4]);
-        } else if (pieces2[12].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[8], pieces2[12]);
-        } 
-    };
-
-    pieces2[9].onclick = function () {  //10
-
-        if (pieces2[10].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[9], pieces2[10]);
-        } else if (pieces2[8].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[9], pieces2[8]);
-        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[9], pieces2[13]);
-        } else if (pieces2[5].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[9], pieces2[5]);
-        }
-    };
-
-    pieces2[10].onclick = function () {  //11
-
-        if (pieces2[11].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[10], pieces2[11]);
-        } else if (pieces2[6].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[10], pieces2[6]);
-        } else if (pieces2[14].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[10], pieces2[14]);
-        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[10], pieces2[9]);
-        }
-    };
-
-    pieces2[11].onclick = function () {  //12
-
-        if (pieces2[10].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[11], pieces2[10]);
-        } else if (pieces2[15].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[11], pieces2[15]);
-        } else if (pieces2[7].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[11], pieces2[7]);
-        } 
-    };
-
-    pieces2[12].onclick = function () {  //13
-
-        if (pieces2[8].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[12], pieces2[8]);
-        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[12], pieces2[13]);
-        } 
-    };
-
-    pieces2[13].onclick = function () { //14
-        if (pieces2[14].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[13], pieces2[14]);
-        } else if (pieces2[12].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[13], pieces2[12]);
-        } else if (pieces2[9].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[13], pieces2[9]);
-        }
-    };
-
-    pieces2[14].onclick = function () { //15
-        if (pieces2[15].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[14], pieces2[15]);
-        } else if (pieces2[13].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[14], pieces2[13]);
-        } else if (pieces2[10].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[14], pieces2[10]);
-        }
-    };
-
-    pieces2[15].onclick = function () { //when a tile moves into the bottom right corner
-        if (pieces2[14].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[15], pieces2[14]);
-        } else if (pieces2[11].outerHTML.includes('id="empty"')) {
-            switchPieces(pieces2[15], pieces2[11]);
-        }
-    };
-    
+    addListener(pieces2);
+   
    
     /*testing*/
 
